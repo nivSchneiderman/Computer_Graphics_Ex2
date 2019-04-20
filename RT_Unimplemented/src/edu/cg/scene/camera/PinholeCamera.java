f6fc416f5919f46162ca3828edd6ea81247b301d
orrzeitler <37776191+orrzeitler@users.noreply.github.com>
2019-04-20 12:12:42 +0300

    befor branch

--- RT_Unimplemented/src/edu/cg/scene/camera/PinholeCamera.java
+++ RT_Unimplemented/src/edu/cg/scene/camera/PinholeCamera.java
@@ -6,11 +6,6 @@ import edu.cg.algebra.Vec;
 
 public class PinholeCamera {
 	//TODO Add your fields
-	Point cameraPosition;
-	Vec towardsVec;
-	Vec upVec;
-	double distanceToPlain;
-	
 	
 	/**
 	 * Initializes a pinhole camera model with default resolution 200X200 (RxXRy) and image width 2.
@@ -20,12 +15,40 @@ public class PinholeCamera {
 	 * @param distanceToPlain - The distance of the camera (position) to the center point of the image-plain.
 	 * 
 	 */
+	
+	//class fieleds 
+	public Point p0;  //camera position
+	public Point imageCenter;
+	public double plainWidth;
+	public double distanceToPlane;
+	public Vec VTowards; //Normalized 
+	public Vec VRight;
+	public Vec Vup;
+	public double pixcelSize;
+	public int imageRes;
+	public int Rx;
+	public int Ry;
+	
+	
 	public PinholeCamera(Point cameraPosition, Vec towardsVec, Vec upVec, double distanceToPlain) {
-		// TODO: Initialize your fields
-		this.cameraPosition = cameraPosition;
-		this.towardsVec = towardsVec;
-		this.upVec = upVec;
-		distanceToPlain = distanceToPlain;
+		
+		//set p0
+		p0 = cameraPosition;
+		
+		//set towards and normalize
+		VTowards = towardsVec.normalize();
+		
+		//find center point 
+		imageCenter = p0.add(distanceToPlain, VTowards);
+		
+		//find V right 
+		VRight = (VTowards.cross(upVec)).normalize();
+		
+		//correct Vup
+		Vup = (VRight.cross(VTowards)).normalize();
+		
+				
+		
 	}
 	/**
 	 * Initializes the resolution and width of the image.
@@ -34,7 +57,11 @@ public class PinholeCamera {
 	 * @param viewPlainWidth - the width of the image plain in world coordinates.
 	 */
 	public void initResolution(int height, int width, double viewPlainWidth) {
-		//TODO: init your fields
+	         
+		Rx = width;
+		Ry= height;
+		pixcelSize = viewPlainWidth /(double)Rx;
+		
 	}
 
 	/**
@@ -44,14 +71,27 @@ public class PinholeCamera {
 	 * @return the middle point of the pixel (x,y) in the model coordinates.
 	 */
 	public Point transform(int x, int y) {
-		throw new UnimplementedMethodException("transform");
+
+		Vec right = VRight.mult(x - Math.floor((double)Rx/2)*pixcelSize);
+		Vec up = Vup.mult(y - Math.floor((double)Ry/2)*pixcelSize);
+		return p0.add(right.add(up.neg()));
+		
 	}
 	
 	/**
 	 * Returns a copy of the camera position
 	 * @return a "new" point representing the camera position.
+	 * @throws IllegalAccessException 
+	 * @throws InstantiationException 
 	 */
-	public Point getCameraPosition() {
-		throw new UnimplementedMethodException("getCameraPosition");
+	public Point getCameraPosition() throws InstantiationException, IllegalAccessException {
+		
+		Point copyOfCameraPosition = Point.class.newInstance();
+		
+		copyOfCameraPosition.x = p0.x;
+		copyOfCameraPosition.y = p0.y;
+		copyOfCameraPosition.z = p0.z;
+		
+		return copyOfCameraPosition; 
 	}
 }
